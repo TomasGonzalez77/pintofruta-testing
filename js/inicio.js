9cfe08dbe4dd2819990b6a4c8d66e7da5b2cd00b
Tomas Gonzalez <gonzaleztomasn@hotmail.com>
2023-05-12 11:50:58 -0300

    "commit"

---
+++ js/inicio.js
@@ -0,0 +1,1 @@
+let bienvenida = prompt("¡Bienvenido a pintofruta! Ingrese su mail si desea recibir informacion acerca de nuestros productos, de lo contrario pulse *Cancelar* para continuar con su compra.")
